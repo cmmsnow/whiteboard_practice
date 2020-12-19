68a075fe8d6e0da6b8cc0076ea0856fc892aa7cb
christine <christamsnow@gmail.com>
2020-12-19 13:35:09 -0500

    added Jays mock whiteboard problem

---
+++ src/main/java/Assorted.java
@@ -0,0 +1,20 @@
+public class Assorted {
+//    Jay's Mock Whiteboard
+//    Given: an array of numbers
+//    Task: add up the sums of all possible sub arrays
+//    Example: {1, 2, 3, 4}
+//	+1
+//            +1+2
+//            +1+2+3
+//            +1+2+3+4
+//            +2
+//            +2+3
+//            +2+3+4
+//            +3
+//            +3+4
+//            +4
+//            =50
+    public int addEmAll(int[] input){
+        return -1;
+    }
+}
---
+++ src/test/java/AssortedTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class AssortedTest {
+    Assorted assorted;
+
+    @Before
+    public void setup(){
+        assorted = new Assorted();
+    }
+
+    @Test
+    public void addEmAllTest(){}
+}
