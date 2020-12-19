ad3af50e5bdd09ffbce19a2a4b16873349a945d5
christine <christamsnow@gmail.com>
2020-12-19 12:52:35 -0500

    added jeremySort class and test to solve later

---
+++ src/main/java/JeremySort.java
@@ -0,0 +1,24 @@
+/**
+ * Get comfortable with bubble sort for situations like this
+ */
+
+public class JeremySort {
+
+//    int [8, 4, 5, 3, 1, 2, 6, 7, 10, 9]
+//    int [4,]
+//    int[] of 10 various numbers:  sort them lowest to highest
+//
+//    public int[] sortMe(int[] input){
+//        int[] answer = new int[10].clone(input);
+//        for (int i=0; i<answer.length-1; i++){
+//            If (answer[i] > answer[i+1]){
+//                answer[i] = answer[i+1];
+//            }
+//        }
+//
+//        //move smaller one to left
+//
+//        Return answer;
+//    }
+
+}
---
+++ src/test/java/JeremySortTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class JeremySortTest {
+    JeremySort jeremySort;
+    int[] arr1;
+    int[] arr2;
+
+    @Before
+    public void setup(){
+        jeremySort = new JeremySort();
+        arr1 = new int[] {5, 4, 3, 6, 7};
+        arr2 = new int[] {-6, -8, -4, 1, -1};
+    }
+
+    @Test
+    public void sortMeTest(){}
+}
