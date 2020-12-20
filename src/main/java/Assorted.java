273df5a5dce6adc64e0f487fb5294a058bb7ca95
christine <christamsnow@gmail.com>
2020-12-20 11:41:49 -0500

    add em all solved with 3 different solutions

--- src/main/java/Assorted.java
+++ src/main/java/Assorted.java
@@ -1,49 +1,51 @@
 public class Assorted {
-//    Jay's Mock Whiteboard
-//    Given: an array of numbers
-//    Task: add up the sums of all possible sub arrays
-//    Example: {1, 2, 3, 4}
-//	+1
-//            +1+2
-//            +1+2+3
-//            +1+2+3+4
-//            +2
-//            +2+3
-//            +2+3+4
-//            +3
-//            +3+4
-//            +4
-//            =50
+/**
+ * AddEmAll:    Jay's Mock Whiteboard
+ *              Given: an array of numbers
+ *              Task: add up the sums of all possible sub arrays
+ *              Example: {1, 2, 3, 4}
+ * 	            +1
+ *              +1+2
+ *              +1+2+3
+ *              +1+2+3+4
+ *              +2
+ *              +2+3
+ *              +2+3+4
+ *              +3
+ *              +3+4
+ *              +4
+ *              =50
+ */
     public int addEmAll(int[] input){
-        int sum = 0;
-//        for (int i=0; i<input.length; i++){
-//            for (int j=0; j<input.length; j++){
-//                if (i != j){
-//                    sum += (input[i] + input[j]);
+        int sum = 0, temp = 0;
+        /**
+         * this works - don't fully get it:
+         */
+        for (int i=0; i<input.length; i++){
+            temp = 0;
+            for (int j=i; j<input.length; j++){
+                temp += input[j];
+                sum += temp;
+            }
+        }
+
+        /**
+         * Shu's answer. This works & I get it:
+         */
+//        for (int i = 0; i < input.length ; i++) {
+//            for(int j= 0; j<input.length; j++){
+//                for(int k=i;k<=j; k++){
+//                    sum += input[k];
 //                }
 //            }
 //        }
-        // not what was asked ^^
 
-
-//        for (int i=0; i<input.length; i++){
-//            for (int j=i; j<input.length; j++){
-//                if (i != j){
-//                    sum += (input[i] + input[j]);
-//                }
-//            }
+        /**
+         * this works but I dont get it at all:
+         */
+//        for (int i=0; i<input.length; i++) {
+//            sum += (input[i] * (i + 1) * (input.length - i));
 //        }
-//        for (int i=input.length-2; i>=0; i--){ //-2 , not -1, bc you have already added 1, 2, 3, 4, the 1st time.
-//            for (int j=i; j>=0; j--){
-//                if (i != j){
-//                    sum += (input[i] + input[j]);
-//                }
-//            }
-//        }
-        // this doesn't quite work either ^^
-
-
-        //Think about checking for string[] palindrome situation
 
         return sum;
     }
