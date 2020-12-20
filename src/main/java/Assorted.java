6c70d5a50d9b14233cfec0606f6c869017f8d699
christine <christamsnow@gmail.com>
2020-12-19 23:06:33 -0500

    thoughts on sum of all sub arrays

--- src/main/java/Assorted.java
+++ src/main/java/Assorted.java
@@ -15,6 +15,36 @@ public class Assorted {
 //            +4
 //            =50
     public int addEmAll(int[] input){
-        return -1;
+        int sum = 0;
+//        for (int i=0; i<input.length; i++){
+//            for (int j=0; j<input.length; j++){
+//                if (i != j){
+//                    sum += (input[i] + input[j]);
+//                }
+//            }
+//        }
+        // not what was asked ^^
+
+
+//        for (int i=0; i<input.length; i++){
+//            for (int j=i; j<input.length; j++){
+//                if (i != j){
+//                    sum += (input[i] + input[j]);
+//                }
+//            }
+//        }
+//        for (int i=input.length-2; i>=0; i--){ //-2 , not -1, bc you have already added 1, 2, 3, 4, the 1st time.
+//            for (int j=i; j>=0; j--){
+//                if (i != j){
+//                    sum += (input[i] + input[j]);
+//                }
+//            }
+//        }
+        // this doesn't quite work either ^^
+
+
+        //Think about checking for string[] palindrome situation
+
+        return sum;
     }
 }
--- src/test/java/AssortedTest.java
+++ src/test/java/AssortedTest.java
@@ -1,14 +1,25 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AssortedTest {
     Assorted assorted;
+    int[] ogArray;
+    int[] array1;
+    int[] array2;
 
     @Before
     public void setup(){
         assorted = new Assorted();
+        ogArray = new int[]{1, 2, 3, 4};
+        array1 = new int[]{1, 3, 2, 4, 1};
+        array2 = new int[]{-5, -1, -3, 2, 3};
     }
 
     @Test
-    public void addEmAllTest(){}
+    public void addEmAllTest(){
+        int expected = 50;
+        int actual = assorted.addEmAll(ogArray);
+        Assert.assertEquals(expected, actual);
+    }
 }
