559a0adc76c3fda5a2c26c4414a54c43c3da5a1b
christine <christamsnow@gmail.com>
2020-12-21 13:15:20 -0500

    removeDupesByShu added and tested

--- src/main/java/Assorted.java
+++ src/main/java/Assorted.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Assorted {
 /**
  * AddEmAll:    Jay's Mock Whiteboard
@@ -19,7 +21,7 @@ public class Assorted {
     public int addEmAll(int[] input){
         int sum = 0, temp = 0;
         /**
-         * this works - don't fully get it:
+         * this works:
          */
         for (int i=0; i<input.length; i++){
             temp = 0;
@@ -30,7 +32,7 @@ public class Assorted {
         }
 
         /**
-         * Shu's answer. This works & I get it:
+         * Shu's answer. This works & makes most sense:
          */
 //        for (int i = 0; i < input.length ; i++) {
 //            for(int j= 0; j<input.length; j++){
@@ -41,7 +43,7 @@ public class Assorted {
 //        }
 
         /**
-         * this works but I dont get it at all:
+         * this works but I don't understand why:
          */
 //        for (int i=0; i<input.length; i++) {
 //            sum += (input[i] * (i + 1) * (input.length - i));
@@ -49,4 +51,129 @@ public class Assorted {
 
         return sum;
     }
+
+    /**
+     * Given: String with only alphabetic characters
+     * Return: highest character
+     * Input: xyzx
+     * Output: z
+     */
+    public char findHighestChar(String str){
+        char answer = str.charAt(0);
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) > answer) answer = str.charAt(i);
+        }
+
+//        char[] strArr = str.toCharArray();  //another option that works
+//        for (int i=0; i<strArr.length; i++){
+//            if (strArr[i] > answer) answer = strArr[i];
+//        }
+
+        return answer;
+    }
+
+    /**
+     * Given an int array, return an array with duplicates removed
+     *
+     * public int[] removeDuplicates(int[] array){  SHU'S ANSWER
+     * //{1,1,2, ,5,3, 5}
+     * int count = 0;
+     * for(int i=0; i< array.length; i++){ //1
+     * for(int j=0; j< array.length; j++){
+     * if((array[i] == array[j]) && (i != j)){
+     * count++;
+     * }
+     * }
+     * int [] result = new int [array.length- count];
+     * int k = 0;
+     * for(int i=0; i< result.length; i++){
+     * for(int j=0; j< array.length; j++){
+     * if(array[i] != array [j]){
+     * result [k] = array[i];
+     * k++;
+     * }
+     * } return result;
+     * }
+     */
+    public int[] createOccurenceCounterArray(int[] input){
+        int[] answer = new int[input.length];
+        for (int i=0; i<input.length; i++){
+            int counter = 1;
+            for (int j=0; j<input.length; j++){
+                if (i != j){
+                    if (input[i] == input[j]){
+                        counter++;
+                    }
+                }
+            }
+            answer[i] = counter;
+        }
+        return answer;
+    }
+
+    public int getNumOfDupesFromOccurrenceCounter(int[] input){
+        int counter = 0;
+        for (int i=0; i<input.length; i++){
+            counter += (input[i] - 1);
+        }
+        return counter;
+    }
+
+    public boolean isDuplicate(int inputNum, int[] inputArr){
+        int counter = 0;
+        for (int i=0; i<inputArr.length; i++){
+            if (inputArr[i] == inputNum) counter++;
+        }
+        if (counter > 1) return true;
+        return false;
+    }
+
+//    public int[] removeDuplicates(int[] input){  // still working on this
+//        int[] dupeCountAr = createOccurenceCounterArray(input);
+//        int numOfDupes = getNumOfDupesFromOccurrenceCounter(dupeCountAr);
+//        int[] answer = new int[input.length - numOfDupes];
+//        for (int i=0; i<input.length; i++){
+//            if (dupeCountAr[i] )
+//        }
+//    }
+
+
+//    public int[] removeDuplicates(int[] array) {  // Shu's 1st answer // does not work
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                if ((array[i] == array[j]) && (i != j)) count++;
+//            }
+//        }
+//        int[] result = new int[array.length - count];
+//        int k = 0;
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                if (array[i] != array[j]) {
+//                    result[k] = array[i];
+//                    k++;
+//                }
+//            }
+//        }
+//        return result;
+
+
+    public int[] removeDuplicatesByShu(int[] array) {  // Shu's 2nd answer -- this removes dupes AND sorts the array
+            HashMap<Integer, Integer> countNumbers = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (countNumbers.containsKey(array[i])){
+                countNumbers.put(array[i], countNumbers.get(array[i]+1));
+            } else {
+                countNumbers.put(array[i], 1);
+            }
+        }
+        int[] result = new int[countNumbers.size()];
+        int j =0;
+        for (Integer i: countNumbers.keySet()) {
+            result[j] = i;
+            j++;
+        }
+        return result;
+    }
+
 }
--- src/test/java/AssortedTest.java
+++ src/test/java/AssortedTest.java
@@ -6,12 +6,20 @@ public class AssortedTest {
     Assorted assorted;
     int[] ogArray;
     int[] array1;
+    String christine;
+    String shubham;
+    int[] dupes1;
+    int[] dupes2;
 
     @Before
     public void setup(){
         assorted = new Assorted();
         ogArray = new int[]{1, 2, 3, 4};
         array1 = new int[]{1, 2, 3};
+        christine = "christine";
+        shubham = "Shubham";
+        dupes1 = new int[]{5, 5, 7, 3, 4, 3};
+        dupes2 = new int[]{1, 2, 1, 3, 2, 3, 0};
     }
 
     @Test
@@ -26,5 +34,67 @@ public class AssortedTest {
         int expected = 20;
         int actual = assorted.addEmAll(array1);
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findHighestCharTest1(){
+        char expected = 't';
+        char actual = assorted.findHighestChar(christine);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findHighestCharTest2(){
+        char expected = 'u';
+        char actual = assorted.findHighestChar(shubham);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDupesByShuTest1(){
+        int[] expected = new int[]{3, 4, 5, 7};
+        int[] actual = assorted.removeDuplicatesByShu(dupes1);
+
+        int expected1 = expected[0];
+        int expected2 = expected[1];
+        int expected3 = expected[2];
+        int expected4 = expected[3];
+        int expectedLength = expected.length;
+
+        int actual1 = actual[0];
+        int actual2 = actual[1];
+        int actual3 = actual[2];
+        int actual4 = actual[3];
+        int actualLength = actual.length;
+
+        Assert.assertEquals(expectedLength, actualLength);
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected3, actual3);
+        Assert.assertEquals(expected4, actual4);
+    }
+
+    @Test
+    public void removeDupesByShuTest2(){
+        int[] expected = new int[]{0, 1, 2, 3};
+        int[] actual = assorted.removeDuplicatesByShu(dupes2);
+
+        int expected1 = expected[0];
+        int expected2 = expected[1];
+        int expected3 = expected[2];
+        int expected4 = expected[3];
+        int expectedLength = expected.length;
+
+        int actual1 = actual[0];
+        int actual2 = actual[1];
+        int actual3 = actual[2];
+        int actual4 = actual[3];
+        int actualLength = actual.length;
+
+        Assert.assertEquals(expectedLength, actualLength);
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected3, actual3);
+        Assert.assertEquals(expected4, actual4);
     }
 }
